2749469a31991dca96e162b3dae161eaa1e71d0a
asimamir <asimamir32@gmail.com>
2025-04-21 22:06:13 +0500

    fixes

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ const PORT = process.env.PORT || 9090;
 
 app.use(
   cors({
-    origin: "https://frontend-5mx4.vercel.app/", // or "http://localhost:3000" or your frontend domain
+    origin: "https://frontend-5mx4.vercel.app/",
   })
 );
 app.use("/api", router);
+app.get("/", function (req, res) {
+  res.send("Hello World");
+});
 app.use(express.json());
 const connectDb = require("./db/db");
 app.listen(PORT, () => {
