9502365e34c0e049301fbbd35ab3a7bffa1745c8
asimamir <asimamir32@gmail.com>
2025-04-21 21:57:49 +0500

    fixes

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const cors = require("cors");
 const router = require("./route/costRoute");
 const app = express();
+const PORT = process.env.PORT || 9090;
+
 app.use(cors());
 app.use("/api", router);
 app.use(express.json());
 const connectDb = require("./db/db");
-app.listen(9090, () => {
+app.listen(PORT, () => {
   console.log("listening on port");
 });
 connectDb();
